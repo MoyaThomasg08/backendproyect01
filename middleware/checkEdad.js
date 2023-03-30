a373a360fe67b1c88cff2da7d5f53a4fbab5c9b5
MoyaThomasg08 <108040758+MoyaThomasg08@users.noreply.github.com>
2023-03-30 11:10:08 -0300

    Update checkEdad.js

--- middleware/checkEdad.js
+++ middleware/checkEdad.js
@@ -2,7 +2,7 @@ const { gimnasio } = require("../models/gimnasio");
 module.exports = checkEdad = async (req, res, next) => {
   try {
     const user = await gimnasio.find(req.params.edad);
-    if (user.edad < 18 && user.edad > 40) {
+    if (user.edad > 18 && user.edad < 40) {
       next();
     } else {
       return res.json({

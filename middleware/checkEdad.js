f3e865a90ce5056ead8b1f7877e1fef00f0ffcaa
MoyaThomasg08 <108040758+MoyaThomasg08@users.noreply.github.com>
2023-04-01 17:41:24 -0300

    Update checkEdad.js

--- middleware/checkEdad.js
+++ middleware/checkEdad.js
@@ -1,8 +1,17 @@
 const { gimnasio } = require("../models/gimnasio");
-module.exports = checkEdad = async (req, res, next) => {
-  const user = await gimnasio.find(req.params.edad);
-  if (parseInt(user.edad) < 18 && parseInt(user.edad) > 40) {
-    return res.status(401).json({ msg: "El usuario necesita entrenador" });
+
+const checkEdad = async (req, res, next) => {
+  try {
+    const usuario = await gimnasio.findById(req.params.id);
+    console.log(usuario.edad);
+    if (isNaN(usuario.edad) || usuario.edad < 18 || usuario.edad > 40) {
+      return res.status(401).json({ msg: "El usuario necesita entrenador" });
+    }
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Error interno del servidor" });
   }
-  next();
 };
+
+module.exports = checkEdad;

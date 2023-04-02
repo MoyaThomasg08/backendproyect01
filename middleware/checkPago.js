106c91e5a30a046976580d19106248998bac958d
MoyaThomasg08 <thomasmoyag.08@gmail.com>
2023-04-02 12:04:31 -0300

    Update checkPago.js

--- middleware/checkPago.js
+++ middleware/checkPago.js
@@ -1,2 +1,13 @@
 const {usuario} = require("../models/usuario")
-module.exports = checkPago=
+module.exports = checkPago = async (req, res, next) =>{
+    try {
+        const cliente = await usuario.findById(req.params.id);
+        if (cliente.pago){
+            next();
+        } else {
+            return res.status (401).json({msg:"El cliente no esta al dia con el pago"})
+        }
+    } catch (error) {
+        res.status(501).json(error);
+    }
+}

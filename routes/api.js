1a57cbdbe14e8ce5be36f708efad668558df08cc
MoyaThomasg08 <thomasmoyag.08@gmail.com>
2023-04-02 12:11:14 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,10 @@ const apiController = require("../controllers/apiController");
 const validarID = require("../middleware/validarID");
 const checkExpressVal = require("../middleware/checkExpressVal");
 const checkEdad = require("../middleware/checkEdad");
+const checkPago = require ("../middleware/checkPago");
 
 router.get("/users", apiController.users);
-router.get("/user/:id", validarID, checkEdad, apiController.user);
+router.get("/user/:id", validarID, checkEdad, checkPago, apiController.user);
 router.post("/create", checkExpressVal, apiController.create);
 router.put("/edit/:id", validarID, checkExpressVal, apiController.edit);
 router.delete("/delete/:id", validarID, apiController.delete);
